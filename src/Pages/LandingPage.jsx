8e4275dc53a39c22a9d3911d61dacb464cbc2ed3
Prakash Mandal <prakashmandal.iv@gmail.com>
2023-02-10 01:30:04 +0530

    Add : started landing page

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,6 +1,13 @@
-const LandingPage=()=>{
-    return(<>
-    
+const LandingPage = () => {
+    return (<>
+        <div className="flex w-full h-[100vh]">
+            <div className="flex flex-1 bg-gradient-to-r from-blue-400 to-blue-700 shadow-2xl shadow-blue-700">
+                <div className="m-auto border border-red-500">
+                  
+                </div>
+            </div>
+            <div className="flex-1"></div>
+        </div>
     </>)
 }
 export default LandingPage
