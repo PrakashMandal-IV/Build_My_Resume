761b36bdc947bc95819052b6d9dc1ef067d5aad0
Prakash Mandal <prakashmandal.iv@gmail.com>
2023-02-10 02:13:43 +0530

    Add : hero section

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -10,7 +10,20 @@ const LandingPage = () => {
 
                 </div>
             </div>
-            <div className="flex-1"></div>
+            <div className="flex  flex-1">
+                <div className="m-auto flex flex-col gap-5 ">
+                <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 ">
+                        <span class="w-full relative md:text-2xl px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-sky-400 rounded-md group-hover:bg-opacity-0">
+                          Start Building
+                        </span>
+                    </button>
+                    <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 ">
+                        <span class="w-full relative md:text-2xl px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-sky-400 rounded-md group-hover:bg-opacity-0">
+                           View Templates
+                        </span>
+                    </button>
+                </div>
+            </div>
         </div>
     </>)
 }
