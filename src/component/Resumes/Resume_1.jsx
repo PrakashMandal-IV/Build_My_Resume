8ef8b71ea2506f66ee645de5425aff54f910e325
Prakash Mandal <prakashmandal.iv@gmail.com>
2023-02-10 22:38:03 +0530

    Add : Printable Content

--- src/component/Resumes/Resume_1.jsx
+++ src/component/Resumes/Resume_1.jsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useRef } from "react";
+import { PrimaryButton } from "../Button";
+import ReactToPrint from "react-to-print";
 
 function Resume_1() {
-  return (
-    <div className="w-full h-full max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl mx-auto p-5">
-      <div className="w-full h-full bg-white border border-gray-400 shadow-md">
-        
-        
-      </div>
-    </div>
-  );
+    const printRef = useRef(null);
+    return (<>
+
+
+            <ReactToPrint
+                trigger={() => <PrimaryButton Name="Print" className="mx-auto" />}
+                content={() => printRef.current}
+            />
+            <div id="printable-area" ref={printRef} className="w-full h-full max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl mx-auto">
+                <div className="w-full h-full bg-white shadow-md">
+                    {/* Header */}
+                    <nav className="bg-slate-400 h-32 md:h-52  print:bg-slate-400 flex">
+                        <div className="flex flex-col w-full my-auto pl-20">
+                           <p className="text-lg font-light ">My Resume</p>
+                           <p className="text-4xl ">Your Name</p>
+                           <p className="text-lg font-light ">Your Profession</p>
+                        </div>
+                        <div className="flex border w-full my-auto">
+                           
+                           </div>
+                    </nav>
+
+                </div>
+            </div>
+       
+    </>
+    );
 }
 
 export default Resume_1;
