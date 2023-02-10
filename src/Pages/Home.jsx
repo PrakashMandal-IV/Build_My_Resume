7d49ee5c5f27486c4e07dc3ac08eaefe5162b330
Prakash Mandal <prakashmandal.iv@gmail.com>
2023-02-10 20:58:17 +0530

    Add : Resume file

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,10 @@ import Resume_1 from "../component/Resumes/Resume_1"
 const Home=()=>{
     return(<>
       <div className="flex">
+        <div className="w-full">
         <Resume_1 />
+        </div>
+       
       </div>
     </>)
 }
