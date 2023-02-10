6712c764b935face9365e39b85e5b9336598b4de
Prakash Mandal <prakashmandal.iv@gmail.com>
2023-02-11 01:46:50 +0530

    Add : Nav bar

--- src/component/Resumes/Resume_1.jsx
+++ src/component/Resumes/Resume_1.jsx
@@ -7,27 +7,31 @@ function Resume_1() {
     return (<>
 
 
-            <ReactToPrint
-                trigger={() => <PrimaryButton Name="Print" className="mx-auto" />}
-                content={() => printRef.current}
-            />
-            <div id="printable-area" ref={printRef} className="w-full h-full max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl mx-auto">
-                <div className="w-full h-full bg-white shadow-md">
-                    {/* Header */}
-                    <nav className="bg-slate-400 h-32 md:h-52  print:bg-slate-400 flex">
-                        <div className="flex flex-col w-full my-auto pl-20">
-                           <p className="text-lg font-light ">My Resume</p>
-                           <p className="text-4xl ">Your Name</p>
-                           <p className="text-lg font-light ">Your Profession</p>
-                        </div>
-                        <div className="flex border w-full my-auto">
-                           
-                           </div>
-                    </nav>
+        <ReactToPrint
+            trigger={() => <PrimaryButton Name="Print" className="mx-auto" />}
+            content={() => printRef.current}
+        />
+        <div id="printable-area" ref={printRef} className="w-full h-full max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl mx-auto">
+            <div className="w-full h-full bg-white shadow-md">
+                {/* Header */}
+                <nav className="bg-gray-300   h-32 md:h-56 flex">
+                    <div className="flex flex-col w-full my-auto pl-10">
+                        <p className="text-sm  font-light ">Resume</p>
+                        <p className="text-2xl ">Your Name</p>
+                        <p className="text-sm font-light ">Your Profession</p>
+                    </div>
+                    <div className="flex w-full my-auto">
+                            <div className="ml-auto flex flex-col gap-3 mr-10">
+                                <p className="font-light text-sm"> <span className="font-medium  ">T :</span> +91 999 8888 000</p>
+                                <p className="font-light text-sm "> <span className="font-medium  ">M :</span> prakashmandal.iv@gmail.com</p>
+                                <p className="font-light text-sm "> <span className="font-medium  ">W :</span> https://larainfo.com/blogs</p>
+                            </div>
+                    </div>
+                </nav>
 
-                </div>
             </div>
-       
+        </div>
+
     </>
     );
 }
