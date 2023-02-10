35990247987a6bf732e12359ba55ff682d646cf0
Prakash Mandal <prakashmandal.iv@gmail.com>
2023-02-10 10:08:17 +0530

    Add : landing page complete

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -5,7 +5,7 @@ const LandingPage = () => {
     const nav = useNavigate()
     return (<>
         <div className="flex w-full h-[100vh]">
-            <div className="flex flex-1 bg-gradient-to-r from-sky-500 to-sky-700 shadow-2xl shadow-sky-700">
+            <div className="flex flex-1 bg-gradient-to-r from-sky-500 to-sky-700 shadow-2xl shadow-sky-700 z-50">
                 <div className="m-auto w-2/3">
 
                     <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"> Build your</h1>
@@ -16,17 +16,9 @@ const LandingPage = () => {
             </div>
             <div className="flex  flex-1 bg-gray-100  ">
                 <div className="m-auto flex flex-col gap-5 ">
-                {/* <button onClick={()=>nav('/home')} class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 ">
-                        <span class="w-full relative md:text-xl px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-sky-400 rounded-md group-hover:bg-opacity-0">
-                          Start Building
-                        </span>
-                    </button>
-                    <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 ">
-                        <span class="w-full relative md:text-xl px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-sky-400 rounded-md group-hover:bg-opacity-0">
-                           View Templates
-                        </span>
-                    </button> */}
+              
                     <PrimaryButton Name={"Start Building"} />
+                    <PrimaryButton Name={"View Templates"} />
                 </div>
             </div>
         </div>
