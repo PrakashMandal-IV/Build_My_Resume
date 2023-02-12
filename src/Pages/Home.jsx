fc7da3149bad49fd00d0a9500ee2d40a67bbcf6b
Prakash Mandal <prakashmandal.iv@gmail.com>
2023-02-12 12:47:13 +0530

    Add : Header

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import Resume_1 from "../component/Resumes/Resume_1"
 
 const Home=()=>{
     return(<>
-      <div className="flex bg-gray-100">
+      <div className="flex">
         <div className=" mx-auto ">
         <Resume_1 />
         </div>
--- src/component/Resumes/Resume_1.jsx
+++ src/component/Resumes/Resume_1.jsx
@@ -4,17 +4,28 @@ import './Resume_1.css'
 import MyData from '../../JsonData.json'
 import { PrimaryButton } from "../Button";
 function Resume_1() {
-    const [Information, SetInformation] = useState(MyData)
-    return (<>
-           
-          <div className="body border mt-5">
+  const [Information, SetInformation] = useState(MyData)
+  return (<>
 
+    <div className="body border mt-5">
+      <div className="header bg-gray-200" style={{ height: "200px" }}>
+        <div className="" style={{ display: "flex", flexDirection: "column", height: "100%", width: "100%" }}>
+          <div className=" " style={{ marginTop: "auto", marginBottom: "auto", display: "flex" }}>
+                <div className="" style={{flexGrow:"1",marginLeft:"50px"}}>
+                          <p className="border" style={{fontSize:"14px"}}>Resume</p>
+                          <p className="" style={{fontSize:"26px",fontWeight:"500",letterSpacing:"2px"}}>{Information.PersonalData.FirstName+" "+Information.PersonalData.LastName}</p>
+                          <p className="" style={{fontSize:"14px"}}>{Information.PersonalData.Profession}</p>
+                </div>
+                <div className="" style={{flexGrow:"1"}}>
+                  
+                  </div>
           </div>
-          <div className="mt-5">
-            <PrimaryButton Name="Print" />
-          </div>
-    </>
-    );
+        </div>
+      </div>
+    </div>
+
+  </>
+  );
 }
 
 export default Resume_1;
