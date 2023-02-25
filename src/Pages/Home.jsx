88960f70a5ebccf03a8cd636dc8f25159261e819
Prakash Mandal <prakashmandal.iv@gmail.com>
2023-02-25 19:17:37 +0530

    Change : nav

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,15 +9,12 @@ const Home = () => {
 
 
   return (<>
-    <div className="flex flex-col md:flex-row h-[100vh-3rem]">
+    {/* <div className="flex flex-col md:flex-row h-[100vh-3rem]">
       <div className="w-1/3  mt-auto ">
       <ReactToPrint
           trigger={() => <PrimaryButton Name="Print"/>}
           content={() => printref.current}
         />
-     
-     
-  
       </div>
       <div className="w-2/3 pt-5 bg-gray-300">
         <div className="mx-auto md:w-[50rem] shadow-md" >
@@ -27,7 +24,9 @@ const Home = () => {
 
       </div>
 
-    </div>
+    </div> */}
+     
+    
   </>)
 }
 export default Home
