14ab5af11c5c95316463735bf95cf5bb853b6e49
Prakash Mandal <prakashmandal.iv@gmail.com>
2023-02-26 01:47:16 +0530

    Add : input box

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,5 @@
 import Resume_1 from "../component/Resumes/Resume_1"
-import { PrimaryButton } from "../component/Button";
+import { InputBox, PrimaryButton } from "../component/Button";
 import ReactToPrint from "react-to-print";
 import { createRef, useRef } from "react";
 import homepageBg from '../Assets/HomepageBG.png';
@@ -27,9 +27,14 @@ const Home = () => {
     </div> */}
     <div className="flex  bg-no-repeat bg-right bg-contain bg-transparent h-1/2  md:bg-cover  md:h-[100vh] md:max-h-[1920px] p-4" style={{ backgroundImage: `url(${homepageBg})` }}>
       <div className="my-auto w-1/2 text-lg px-10 flex flex-col gap-10">
-         <p className="w-2/3">Elevate your job search with our user-friendly website. With multiple intuitive designs and templates, you can effortlessly create a professional resume for free. Impress potential employers and land your dream job today!</p>
-        <div className="flex">
-        <input type="text" className="border w"  />
+        <p className="w-2/3">Elevate your job search with our user-friendly website. With multiple intuitive designs and templates, you can effortlessly create a professional resume for free. Impress potential employers and land your dream job today!</p>
+        <div className="flex gap-10">
+          <div className="w-2/5">
+           
+           <InputBox className="h-10 w-full " placeholder="Tell us your Name" />
+           
+          </div>
+          <PrimaryButton Name="Get Started" className="h-10 mt-auto font-semibold   "/>
         </div>
       </div>
     </div>
@@ -41,8 +46,8 @@ const Home = () => {
 export default Home
 
 
-const HeroSection=()=>{
+const HeroSection = () => {
   return (<>
-  <div className="">Home</div>
+    <div className="">Home</div>
   </>)
 }
