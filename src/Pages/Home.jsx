4e52b4781d3baf3744300e4174f3bb47c2093b63
Prakash Mandal <prakashmandal.iv@gmail.com>
2023-02-26 12:31:50 +0530

    Add : scroll animation on nav

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -25,9 +25,9 @@ const Home = () => {
       </div>
 
     </div> */}
-    <div className="flex  bg-no-repeat bg-right bg-contain bg-transparent h-1/2  md:bg-cover  md:h-[100vh] md:max-h-[1920px] p-4" style={{ backgroundImage: `url(${homepageBg})` }}>
-      <div className="my-auto w-1/2 text-lg px-10 flex flex-col gap-10">
-        <p className="w-2/3">Elevate your job search with our user-friendly website. With multiple intuitive designs and templates, you can effortlessly create a professional resume for free. Impress potential employers and land your dream job today!</p>
+    <div className="hidden lg:flex  bg-no-repeat bg-right    bg-transparent h-1/2  md:bg-cover  md:h-[100vh] md:max-h-[1920px] p-4" style={{ backgroundImage: `url(${homepageBg})` }}>
+      <div className="my-auto w-1/2  px-10 flex flex-col gap-10">
+        <p className="w-2/3 ">Elevate your job search with our <b> user-friendly website</b>. With multiple intuitive designs and templates, you can effortlessly create a professional <b>resume for free</b> . Impress potential employers and land your dream job today!</p>
         <div className="flex gap-10">
           <div className="w-2/5">
            
@@ -38,7 +38,21 @@ const Home = () => {
         </div>
       </div>
     </div>
-
+    <div className="flex lg:hidden md:bg-cover h-[50vh]">
+      <div className="mt-auto px-10 flex flex-col gap-10">
+      <p className=" text-sm text-center mx-auto ">Elevate your job search with our <b> user-friendly website</b>. With multiple intuitive designs and templates, you can effortlessly create a professional <b>resume for free</b> . Impress potential employers and land your dream job today!</p>
+      
+          <div className="flex gap-10  mx-auto">
+          <div className="">
+           
+           <InputBox className="h-10 w-full text-sm" placeholder="Tell us your Name" />
+           
+          </div>
+          <PrimaryButton Name="Get Started" className="h-10 mt-auto font-semibold   "/>
+        </div>
+        
+      </div>
+    </div>
     <div className="h-[60vh]"></div>
 
   </>)
--- src/component/nav.jsx
+++ src/component/nav.jsx
@@ -1,10 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom"
 import { Logo } from "./Icons";
 const Nav = () => {
-
+    const [navVisible, setNavVisible] = useState(true);
+    useEffect(() => {
+        const handleScroll = () => {
+          if (window.pageYOffset > 0 && navVisible) {
+            setNavVisible(false);
+          } else if (window.pageYOffset === 0 && !navVisible) {
+            setNavVisible(true);
+          }
+        };
+      
+        window.addEventListener('scroll', handleScroll);
+      
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+      }, [navVisible]);
+      
     return (<>
-        <div className="fixed top-0 w-full flex-col md:flex-row flex gap-5 md:gap-20 m-4">
+        <div className={`fixed top-0 w-full flex-col md:flex-row flex gap-5 md:gap-20 m-4 transition-all  ${navVisible ? '' : ' -translate-y-[100px]  '}`}>
             <Logo className="h-14 ml-0" />
             <div className="flex gap-20 justify-center ">
                 <div className=" text-lg flex justify-center align-middle cursor-pointer hover:text-blue-600 transition-all">
