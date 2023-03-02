2851731afca88ee673096fa16b6c814c4b12e359
Prakash Mandal <prakashmandal.iv@gmail.com>
2023-03-03 00:15:48 +0530

    Add : About yourself

--- src/Pages/CreateData.jsx
+++ src/Pages/CreateData.jsx
@@ -1,59 +1,64 @@
 import { useEffect, useState } from "react"
 import { PrimaryButton } from "../component/Button";
 import styled from 'styled-components'
-const CreateData=()=>{
+const CreateData = () => {
     const searchParams = new URLSearchParams(window.location.search);
-  
-  const [Form,SetForm] = useState(1)
-  const [Scale,SetScale] = useState('scale-0')
-useEffect(()=>{
-     SetUserName(searchParams.get('Name'))
-     setTimeout(() => {
-         SetScale('')
-     }, 200);
-},[])
 
-//UserData
-const [UserName,SetUserName] = useState('')
-const [Profession,SetProfession] = useState('')
+    const [Form, SetForm] = useState(1)
+    const [Scale, SetScale] = useState('scale-0')
+    useEffect(() => {
+        SetUserName(searchParams.get('Name'))
+        setTimeout(() => {
+            SetScale('')
+        }, 200);
+    }, [])
 
-function NextClick(){
-    SetScale('scale-0')
-    setTimeout(() => {
-        SetForm(Form+1)
-        SetScale('')
-    }, 200);
-}
+    //UserData
+    const [UserName, SetUserName] = useState('')
+    const [Profession, SetProfession] = useState('')
+    const [AboutMe,SetAboutMe] = useState('')
+    function NextClick() {
+        SetScale('scale-0')
+        setTimeout(() => {
+            SetForm(Form + 1)
+            SetScale('')
+        }, 200);
+    }
 
-function BackClick(){
-    SetScale('scale-0')
-    setTimeout(() => {
-        SetForm(Form-1)
-        SetScale('')
-    }, 200);
-}
+    function BackClick() {
+        SetScale('scale-0')
+        setTimeout(() => {
+            SetForm(Form - 1)
+            SetScale('')
+        }, 200);
+    }
     return (<>
-            <div className="flex flex-col w-full max-w-[1920px] h-[55vh] mt-[30vh]">
-                {/* {"Intro"} */}
-                <div className={"mx-auto w-full transition-transform flex flex-col "+Scale}>
-                  {Form===1&&(<>
-                      <div className="mx-auto text-center flex flex-col gap-4">
-                      <p className=" text-lg">Welcome, <b> {UserName}</b>, Good to see you today</p>
-                       <p className=" text-lg">Let's start Building your Resume !!</p>
-                       <p className="text-lg font-bold mt-5">Tell us your Profession</p>
-                       <InputBox placeholder="Designer,Programmer,Artist etc" value={Profession} onChange={(e)=>SetProfession(e.target.value)}/>
-                      </div>
-                  </>)}
-                  {Form===2&&(<>
-                       <p className="text-center text-lg">Welcome, <b> {UserName}</b>, Good to see you today</p>
-                       <p className="text-center text-lg">Let's start Building your Resume !!</p>
-                  </>)}
-                  </div>
-                  <div className="mx-auto mt-auto flex gap-20">
-                    {Form!==1&&(<PrimaryButton Name="Back"  onClick={()=>BackClick()} className="w-36" />)}
-                      <PrimaryButton Name="Next" onClick={()=>NextClick()} className="w-36 transition-all" />
-                  </div>
+        <div className="flex flex-col w-full max-w-[1920px] h-[60vh] md:max-h-[1920px] mt-[30vh]">
+            {/* {"Intro"} */}
+            <div className={"mx-auto w-full transition-transform flex flex-col px-10 h-auto mb-10" + Scale}>
+                {Form === 1 && (<>
+                    <div className="mx-auto text-center flex flex-col gap-4 ">
+                        <p className=" text-lg">Welcome, <b> {UserName}</b>, Good to see you today</p>
+                        <p className=" text-lg">Let's start Building your Resume !!</p>
+                        <p className="text-lg font-bold mt-5">Tell us your Profession</p>
+                        <InputBox placeholder="Designer,Programmer,Artist etc" value={Profession} onChange={(e) => SetProfession(e.target.value)} />
+                        <p className="text-center w-full md:w-[40rem] mt-5">Choosing the right profession name for your resume is important for accurately
+                            reflecting your skills and experience. Use industry-standard titles, highlight your skills,
+                            avoid exaggeration, be specific, and tailor your title to the job.</p>
+                    </div>
+                </>)}
+                {Form === 2 && (<>
+                    <div className="mx-auto text-center flex flex-col gap-4">
+                        <p className="text-lg font-bold mt-5">Alright, How will u introduce your self?</p>
+                        <textarea placeholder="About Yourself !!" style={{boxShadow:"-10px -10px 30px 0 #fff, 10px 10px 30px 0 #1d0dca17"}} className="p-2 rounded-md h-36 w-96 outline-none bg-[#f5f5fa]" value={AboutMe} onChange={(e) => SetAboutMe(e.target.value)} />
+                    </div>
+                </>)}
             </div>
+            <div className="mx-auto mt-auto flex gap-20 mb-10">
+                {Form !== 1 && (<PrimaryButton Name="Back" onClick={() => BackClick()} className="w-36" />)}
+                <PrimaryButton Name="Next" onClick={() => NextClick()} className="w-36 transition-all" />
+            </div>
+        </div>
     </>)
 }
 
@@ -93,3 +98,4 @@ word-spacing: normal;
 }
 
 `
+
