809e5bddbd7350bc270590683e9a965f4f66c881
Prakash Mandal <prakashmandal.iv@gmail.com>
2023-03-02 23:36:29 +0530

    Add : Animation on next click

--- src/Pages/CreateData.jsx
+++ src/Pages/CreateData.jsx
@@ -17,21 +17,39 @@ useEffect(()=>{
 const [UserName,SetUserName] = useState()
 
 
+function NextClick(){
+    SetScale('scale-0')
+    setTimeout(() => {
+        SetForm(Form+1)
+        SetScale('')
+    }, 200);
+}
 
+function BackClick(){
+    SetScale('scale-0')
+    setTimeout(() => {
+        SetForm(Form-1)
+        SetScale('')
+    }, 200);
+}
     return (<>
             <div className="flex flex-col w-full max-w-[1920px] h-[55vh] mt-[30vh]">
                 {/* {"Intro"} */}
                 <div className={"mx-auto w-full transition-transform "+Scale}>
                   {Form===1&&(<>
-                   
                        <p className="text-center text-lg">Welcome, <b> {UserName}</b>, Good to see you today</p>
                        <p className="text-center text-lg">Let's start Building your Resume !!</p>
-                  
+                       
+                  </>)}
+                  {Form===2&&(<>
+                       <p className="text-center text-lg">Welcome, <b> {UserName}</b>, Good to see you today</p>
+                       <p className="text-center text-lg">Let's start Building your Resume !!</p>
+                       
                   </>)}
                   </div>
                   <div className="mx-auto mt-auto flex gap-20">
-                    {Form!==1&&(<PrimaryButton Name="Back"  onClick={()=>SetForm(Form-1)} className="w-36" />)}
-                      <PrimaryButton Name="Next" onClick={()=>SetForm(Form+1)} className="w-36 transition-all" />
+                    {Form!==1&&(<PrimaryButton Name="Back"  onClick={()=>BackClick()} className="w-36" />)}
+                      <PrimaryButton Name="Next" onClick={()=>NextClick()} className="w-36 transition-all" />
                   </div>
             </div>
     </>)
