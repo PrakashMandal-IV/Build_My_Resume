0a99a36073385cc13ece7ccb55c12a15bc3687d7
Prakash Mandal <prakashmandal.iv@gmail.com>
2023-03-05 20:45:27 +0530

    Experiments

--- src/Pages/GenerateResume.jsx
+++ src/Pages/GenerateResume.jsx
@@ -1,29 +1,43 @@
+
 import { useEffect, useRef, useState } from "react"
-import ReactToPrint from "react-to-print"
+import { useReactToPrint } from "react-to-print/lib"
 import { PrimaryButton } from "../component/Button"
 import PreviewList, { ResumePreviewList } from "../component/ResumePreviewList"
 import Resume_1 from "../component/Resumes/Resume_1"
-
 const GenerateResume = (props) => {
 
     const printref = useRef()
- 
+
+    function ImageSelectHandler(imgNumber) {
+        handlePrint()
+    }
+    const handlePrint = useReactToPrint({
+        content: () => printref.current,
+    });
     return (<>
-        <div className="mt-[10vh]">
-            <div className="flex flex-col md:flex-row h-[calc(90vh)]">
-                <div className="w-1/3 overflow-x-hidden overflow-y-scroll scroll-smooth ImagePrevSlider">
-                   
-                   <PreviewList />
+        <div className="mt-[15vh] lg:mt-[10vh] mb-10ld:h-full">
+            <div className="flex flex-col lg:flex-row gap-10  lg:h-[90vh]">
+
+                <div className=" lg:w-1/3 ">
+                    <p className="text-center text-lg font-semibold p-2 ">Select Designs</p>
+                    <div className="h-full overflow-x-hidden overflow-y-scroll scroll-smooth ImagePrevSlider">
+                    <PreviewList ImageSelect={(ImageNumber) => ImageSelectHandler(ImageNumber)} />
+                    </div>
                     {/* <ReactToPrint
                          trigger={() => <PrimaryButton Name="Print"/>}
                          content={() => printref.current}
                         /> */}
                 </div>
-                {/* <div className="w-2/3 pt-5 bg-gray-300"> 
-                     <div className="mx-auto md:w-[50rem] shadow-md" >
-                     <Resume_1 printref={printref}/>
-                     </div>
-                       </div> */}
+                <div className=" flex-grow flex flex-col gap-10  py-5 overflow-x-hidden overflow-y-scroll">
+                    <div className="mx-auto">
+                    <PrimaryButton Name="Print" className="w-40" onClick={handlePrint}/>
+                      
+                       
+                    </div>
+                    <div className="hidden md:block mx-auto md:w-[50rem] shadow-md" >
+                        <Resume_1 printref={printref} />
+                    </div>
+                </div>
 
             </div>
         </div>
--- src/component/ResumePreviewList.jsx
+++ src/component/ResumePreviewList.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import Resume_1 from "../Assets/ResumePrevews/Resume_1.jpg"
+import Resume_1 from "../Assets/ResumePrevews/Resume_1.png"
 import Resume_2 from "../Assets/ResumePrevews/Resume_2.jpg"
 import Resume_3 from "../Assets/ResumePrevews/Resume_3.jpg"
 import Resume_4 from "../Assets/ResumePrevews/Resume_4.jpg"
@@ -31,14 +31,16 @@ const PreviewList = (props) => {
 
 
     return (<>
-        <div className="flex flex-col gap-5   px-4 ">
+        <div className="flex  flex-col gap-5   px-4  ">
+           
             {List.map((item,idx)=>(
-                        <div className="flex flex-col gap-4 p-4" key={idx}>
-                        <div className="w-3/5 mx-auto">
-                             <img src={item.img} alt="" />
+                        <div className=" cursor-pointer w-4/5 lg:w-3/5 mx-auto flex flex-col gap-4 p-4 rounded-md  hover:bg-gradient-to-br from-sky-400 to-green-200 transition-[background-color]  duration-300 ease-out" key={idx}>
+                        <div className="">
+                          <img src={item.img} alt="" className="rounded-md w-full" />
                         </div>
-                        <p className="text-center">Image Name</p>
-                    </div>
+                        <p className="text-center">{item.name}</p>
+                      </div>
+                      
                    ))}
 
           
