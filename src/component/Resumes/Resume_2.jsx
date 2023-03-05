7ee50620c84f17c2bc8ddc20d13e5b46fe64013a
Prakash Mandal <prakashmandal.iv@gmail.com>
2023-03-06 03:08:44 +0530

    Add : User Details

--- src/component/Resumes/Resume_2.jsx
+++ src/component/Resumes/Resume_2.jsx
@@ -27,8 +27,35 @@ function Resume_2(props) {
                                 </div>
                             </div>
                             <div className="w-1/2">
-                                <div className=" h-2/5 bg-black  ">
+                                <div className="pl-10 h-2/5 bg-black flex flex-col  ">
+                                    <div className="mt-20">
+                                        <p className="text-5xl text-white">{Information?.PersonalData?.FirstName}</p>
+                                        <p className="text-5xl text-white">{Information?.PersonalData?.LastName}</p>
+                                        <p className="mt-5 text-2xl text-white">{Information?.PersonalData?.Profession}</p>
+                                    </div>
+                                    <div className="mt-20 flex flex-col gap-5">
+                                        <div className="flex gap-4">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-5 h-5">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+                                            </svg>
+                                            <p className="text-white text-sm  font-light">{Information?.PersonalData?.Contact?.Email}</p>
+                                        </div>
+                                        <div className="flex gap-4">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-5 h-5">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+                                            </svg>
 
+                                            <p className="text-white text-sm font-light">{Information?.PersonalData?.Contact?.Phone}</p>
+                                        </div>
+                                        <div className="flex gap-4">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-5 h-5">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M12.75 3.03v.568c0 .334.148.65.405.864l1.068.89c.442.369.535 1.01.216 1.49l-.51.766a2.25 2.25 0 01-1.161.886l-.143.048a1.107 1.107 0 00-.57 1.664c.369.555.169 1.307-.427 1.605L9 13.125l.423 1.059a.956.956 0 01-1.652.928l-.679-.906a1.125 1.125 0 00-1.906.172L4.5 15.75l-.612.153M12.75 3.031a9 9 0 00-8.862 12.872M12.75 3.031a9 9 0 016.69 14.036m0 0l-.177-.529A2.25 2.25 0 0017.128 15H16.5l-.324-.324a1.453 1.453 0 00-2.328.377l-.036.073a1.586 1.586 0 01-.982.816l-.99.282c-.55.157-.894.702-.8 1.267l.073.438c.08.474.49.821.97.821.846 0 1.598.542 1.865 1.345l.215.643m5.276-3.67a9.012 9.012 0 01-5.276 3.67m0 0a9 9 0 01-10.275-4.835M15.75 9c0 .896-.393 1.7-1.016 2.25" />
+                                            </svg>
+
+
+                                            <a href={Information?.PersonalData?.Contact.Website} target="_blank" className="text-white text-sm font-light" >My Website</a>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
