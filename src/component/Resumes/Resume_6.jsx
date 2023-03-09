a28a5a8dc5e998047b44e459898ff409301878be
Prakash Mandal <prakashmandal.iv@gmail.com>
2023-03-09 13:32:11 +0530

    Add : Left side with Image

--- src/component/Resumes/Resume_6.jsx
+++ src/component/Resumes/Resume_6.jsx
@@ -19,7 +19,13 @@ function Resume_6(props) {
         {Information && (
             <div id="printable-area" ref={props.printref} className="body  flex " size="A4">
                 {/* body */}
-                
+                <div className="w-2/5 h-full bg-gradient-to-b from-[#333a4c]  to-[#495168] border flex flex-col">
+                    <div className=" h-60 mt-10 flex justify-center align-middle">
+                        <div className="w-48 h-48 rounded-full overflow-hidden">
+                            <img src={Information?.PersonalData?.ProfilePic} className="object-contain" alt="" />
+                        </div>
+                    </div>
+                </div>
 
             </div>
         )}
