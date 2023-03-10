f7e380354de4e9f512d2f684a8aba4427e8891bb
Prakash Mandal <prakashmandal.iv@gmail.com>
2023-03-10 14:04:49 +0530

    Add : Images

--- src/component/Resumes/Resume_8.jsx
+++ src/component/Resumes/Resume_8.jsx
@@ -19,7 +19,19 @@ function Resume_8(props) {
         {Information && (
             <div id="printable-area" ref={props.printref} className="body bg-gray-50  flex  " size="A4">
                 {/* body */}
-               
+                <div className="flex h-full">
+                    {/* Right */}
+                     <div className="w-1/2  flex">
+                               <div className="w-3/4 mx-auto flex flex-col ">
+                                  {/* Image */}
+                                   <div className="h-2/6 rounded-b-full flex p-5 bg-pink-300">
+                                         <div className="w-full  aspect-square  mt-auto  rounded-full overflow-hidden">
+                                            <img src={Information?.PersonalData?.ProfilePic} alt="" className=" object-contain"/>
+                                         </div>
+                                   </div>
+                               </div>
+                     </div>
+                </div>
             </div>
         )}
 
